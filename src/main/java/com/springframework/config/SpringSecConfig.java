b9ea53051d23d92f787b5ea67b472ae2e7c612d0
DxcGSS <yksrivastav11194@gmail.com>
2021-01-21 11:52:05 +0530

    demo

--- src/main/java/com/springframework/config/SpringSecConfig.java
+++ src/main/java/com/springframework/config/SpringSecConfig.java
@@ -9,9 +9,12 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/","/swagger-resources").permitAll();
+        httpSecurity
+        .authorizeRequests()
+        .antMatchers("/","/swagger-resources").permitAll();
         httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+        httpSecurity.headers()
+        .frameOptions().disable();
     }
 
 
